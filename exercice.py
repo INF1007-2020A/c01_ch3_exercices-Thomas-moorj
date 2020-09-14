e8d985a521b45472fb42efe33245c7ee38a8687b
Thomas-moorj <thomas.moorjani-houle@polymtl.ca>
2020-09-14 18:11:14 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -10,7 +10,7 @@ def average(a: float, b: float, c: float) -> float:
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     
-    return 0
+    return (100 * math.pi)/180
 
 
 def to_degrees(angle_rads: float) -> tuple:
@@ -18,7 +18,7 @@ def to_degrees(angle_rads: float) -> tuple:
 
 
 def to_celsius(temperature: float) -> float:
-    return (100 * math.pi)/180
+    return 
 
 
 def to_farenheit(temperature: float) -> float:
