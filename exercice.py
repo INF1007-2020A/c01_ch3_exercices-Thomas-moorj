9105c9a17272073737b526f4390f1e0aca63f051
Thomas-moorj <thomas.moorjani-houle@polymtl.ca>
2020-09-14 18:44:54 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,9 +18,6 @@ def to_degrees(angle_rads: float) -> tuple:
     degrees= math.degrees (angle_rads)
     
     minutes= (degrees)[0] * 60
-
-    
-    
    
     seconds= math.modf(minutes)[0] * 3600
    
