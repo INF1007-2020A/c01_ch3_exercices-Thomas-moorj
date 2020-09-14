d8b8a76bc234f3ac88739316bab8c731070994d3
Thomas-moorj <thomas.moorjani-houle@polymtl.ca>
2020-09-14 18:39:13 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -25,11 +25,11 @@ def to_degrees(angle_rads: float) -> tuple:
 
 def to_celsius(temperature: float) -> float:
     
-    return to_celsius/1.8 -32.0
+    return (to_celsius/1.8 -32.0)
 
 
 def to_farenheit(temperature: float) -> float:
-    return to_farenheit * 1.8 +32.0
+    return (to_farenheit * 1.8 +32.0)
 
 
 def main() -> None:
